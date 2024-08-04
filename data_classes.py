7f31aea2215cfd1dace1cd34ea57666fdb453f67
Salsabeelyf <salsabeelsay@gmail.com>
2024-08-05 00:31:07 +0100

    Add posittion and portfolio classes

--- data_classes.py
+++ data_classes.py
@@ -16,3 +16,21 @@ class Stock:
             self.price = price_info['price']
             self.currency = price_info['currency']
             self.usd_price = price_info['usd_price']
+
+@dataclass
+class Position:
+    stock: Stock
+    quantity: int
+
+
+@dataclass
+class Portfolio:
+    positions: list[Position]
+
+    def get_total_value(self):
+        total_value = 0
+
+        for position in self.positions:
+            total_value += position.stock.usd_price * position.quantity
+
+        return round(total_value,2)
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import helpers
-import data_classes
+from data_classes import *
 
 def run():
-    print(data_classes.Stock('GOOGL', 'NASDAQ'))
+    bns = Stock('BNS','TSE')
+    google = Stock('GOOGL', 'NASDAQ')
+    shop = Stock('SHOP','TSE')
+    msft = Stock('MSFT','NASDAQ')
+
+    portfolio = Portfolio([Position(bns,100), Position(google,30), Position(shop,10), Position(msft,2)])
+    print(portfolio.get_total_value())
+
 
 if __name__ == '__main__':
     run()
